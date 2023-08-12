936b76b4b932ef2bb96af18b76f64c6756733788
Peter G. Morales <pgmorales76@gmail.com>
2023-08-12 19:47:21 -0400

    Adds books route.

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-'use strict';
+"use strict";
+const getBooks = require("./routes/get-books");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_CONNECT);
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => console.log("mongoose connected"));
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+app.get("/books", getBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
