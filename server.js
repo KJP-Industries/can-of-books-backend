966a096ffbcd537638066817a62a6e718798b364
Josh Easley <easleyjs@gmail.com>
2023-08-15 03:03:59 -0700

    Adds POST Route createBooks. Accepts JSON form (body) data.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 "use strict";
 const getBooks = require("./routes/get-books");
+const createBooks = require("./routes/create-books");
 require("dotenv").config();
 const express = require("express");
+var bodyParser = require('body-parser'); // added body-parser for handling form data
 const cors = require("cors");
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_CONNECT);
@@ -13,9 +15,19 @@ db.once("open", () => console.log("mongoose connected"));
 
 const app = express();
 app.use(cors());
+// for parsing application/json
+app.use(bodyParser.json()); 
 
 const PORT = process.env.PORT || 3001;
 
 app.get("/books", getBooks);
+
+/*
+app.post("/books", (req, res) => {
+    console.log(req.body.title)
+    res.status(201).send('Recieved request.')
+}); //createBooks
+*/
+app.post("/books", createBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
